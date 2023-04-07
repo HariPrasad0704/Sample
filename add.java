7760f456791424e448c2d55106191f71eab5d5f2
HariPrasad <0412hariprasad@gmail.com>
2023-04-07 15:48:09 +0530

    Addition Class is added in add.java

--- add.java
+++ add.java
@@ -4,5 +4,14 @@ int a=10;
 int b=20;
 int c=a+b;
 System.out.println(c);
+System.out.println("Additon class:");
+Addition  ad =new Addition();
+ad.add(30, 40);
 }
+}
+class Addition{
+	
+	public void add(int a,int b) {
+		System.out.println(a+b);
+	}
 }
